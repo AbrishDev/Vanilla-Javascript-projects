2311cad5cbab68772c6fe17ec36b7840ccb8896f
abrham-desalegn <abrhamdesalegn961@gmail.com>
2024-03-25 14:52:33 +0300

    commit on script file

---
+++ PigGame/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+// Selecting elements
+const score1E1 = document.querySelector('#score--0');
+const score2E2 = document.getElementById('score--1');
+const diceE1 = document.querySelector('.dice');
+
+score1E1.textContent = 0;
+score2E2.textContent = 0;
+diceE1.classList.add('hidden');
